23e5b4fdf08ad37e753a45076411fdbcc080a084
armanavasthi <avasthiarman@gmail.com>
2021-10-02 22:42:03 +0530

    First Commit

---
+++ link-storage-service/src/main/java/com/linkhub/linkstorageservice/LinkStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.linkhub.linkstorageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkStorageServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LinkStorageServiceApplication.class, args);
+  }
+
+}
---
+++ link-storage-service/src/test/java/com/linkhub/linkstorageservice/LinkStorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkhub.linkstorageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkStorageServiceApplicationTests {
+
+//  @Test
+//  void contextLoads() {
+//  }
+
+}
